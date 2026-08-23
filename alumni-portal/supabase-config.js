98c9240f8fb00915404379bc8884dd8dd64d1f79
makkimax88-prog <makkimax88@gmail.com>
2026-08-23 16:41:11 +0800

    Add files via upload

---
+++ alumni-portal/supabase-config.js
@@ -0,0 +1,4 @@
+// Paste values from Supabase Project Settings > API.
+window.SNHS_SUPABASE_URL = 'https://nydskqjkfzrnoxpnizgc.supabase.co';
+window.SNHS_SUPABASE_ANON_KEY = 'sb_publishable_T6ixRkOxGAx0BL1AbHNWbA_J0svZfoc';
+window.SNHS_ADMIN_EMAIL = 'raulvillamon01@gmail.com';
